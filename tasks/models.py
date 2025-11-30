108204f4b4666f01c1f20c442bd6f97196ebbe0e
codingwithrsn33 <darekarrohan3377@gmail.com>
2025-12-01 00:02:51 +0530

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    PRIORITY_CHOICES=(
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    )
+
+    STATUS_CHOICES = (
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    )
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    assigned_to = models.ForeignKey(
+        User ,
+        on_delete = models.SET_NULL,
+        null = True ,
+        blank =True,
+        related_name ='assigned_tasks'
+    )
+    
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    due_date = models.DateField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
