603f774ffb32db6bfb7ea43cc19c4cc7b9c2a470
codingwithrsn33 <darekarrohan3377@gmail.com>
2025-12-02 00:24:53 +0530

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
